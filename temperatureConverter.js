98e7c90293a645f17d306be42bee53251e84e303
Ananthadatta02 <sarvadeananthadatta143@gmail.com>
2025-01-15 19:18:19 +0530

    Update on js file

--- temperatureConverter.js
+++ temperatureConverter.js
@@ -25,6 +25,21 @@ function convertFromFahrenheit(){
     if (fahrenheit != ""){
         document.getElementById("celsiusInp").value = (Number(fahrenheit)-32)*5/9;
         document.getElementById("kelvinInp").value = (Number(fahrenheit)-32)*5/9+273.15;
+        if(fahrenheit>=32 && fahrenheit<=84){
+            document.body.style.backgroundColor = "lightblue"
+        }
+        else if(fahrenheit>=85 && fahrenheit<=103){
+            document.body.style.backgroundColor = "lightgreen"
+        }
+        else if(fahrenheit>=104 && fahrenheit<=111){
+            document.body.style.backgroundColor = "lightsalmon"
+        }
+        else if(fahrenheit>=112 && fahrenheit<=112){
+            document.body.style.backgroundColor = "rgb(255, 71, 71)"
+        }
+        else{
+            document.body.style.backgroundColor = "red"
+        }
     }
 }
 function convertFromKelvin(){
@@ -32,5 +47,20 @@ function convertFromKelvin(){
     if (kelvin != ""){
         document.getElementById("celsiusInp").value = (Number(kelvin)-273.15);
         document.getElementById("fahrenheitInp").value = (Number(kelvin)-273.15)*9/5+32;
+        if(kelvin>=273 && kelvin<=302){
+            document.body.style.backgroundColor = "lightblue"
+        }
+        else if(kelvin>=303 && kelvin<=312){
+            document.body.style.backgroundColor = "lightgreen"
+        }
+        else if(kelvin>=313 && kelvin<=317){
+            document.body.style.backgroundColor = "lightsalmon"
+        }
+        else if(kelvin>=318 && kelvin<=323){
+            document.body.style.backgroundColor = "rgb(255, 71, 71)"
+        }
+        else{
+            document.body.style.backgroundColor = "red"
+        }
     }
 }
